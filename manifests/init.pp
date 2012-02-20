ef10a2c7483724bf7b435fca9ed9b852fa327eca
Ken Rimey <ken.rimey@codento.com>
2012-02-20 21:22:57 +0200

    Move nginx configuration into a module.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,18 +1,1 @@
-package { 'nginx':
-    name    => 'nginx-light',
-    ensure  => installed,
-}
-
-file { 'www':
-    ensure  => directory,
-    path    => '/usr/share/nginx/www',
-    source  => '/etc/puppet/private/www',
-    recurse => true,
-    require => Package['nginx'],
-}
-
-service { 'nginx':
-    ensure  => running,
-    enable  => true,
-    require => File['www'],
-}
+class { nginx: }
---
+++ modules/nginx/manifests/init.pp
@@ -0,0 +1,20 @@
+class nginx {
+    package { 'nginx':
+        name    => 'nginx-light',
+        ensure  => installed,
+    }
+
+    file { 'www':
+        ensure  => directory,
+        path    => '/usr/share/nginx/www',
+        source  => '/etc/puppet/private/www',
+        recurse => true,
+        require => Package['nginx'],
+    }
+
+    service { 'nginx':
+        ensure  => running,
+        enable  => true,
+        require => File['www'],
+    }
+}
